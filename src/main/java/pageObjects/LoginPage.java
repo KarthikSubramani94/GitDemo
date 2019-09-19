097bea4cbbb8d38225199c785f558cb469c318fe
karthik <karthik.subramani@sumerusolutions.com>
2019-09-19 17:10:30 +0530

    New Changes in Develop Mode

--- src/main/java/pageObjects/LoginPage.java
+++ src/main/java/pageObjects/LoginPage.java
@@ -3,45 +3,39 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 //rahulonlinetutor@gmail.com
 public class LoginPage {
 
-	
 	public WebDriver driver;
-	
-	By email=By.cssSelector("[id='user_email']");
-	By password=By.cssSelector("[type='password']");
-	By login=By.cssSelector("[value='Log In']");
-	
-	
-	
-	
+
+	By email = By.cssSelector("[id='user_email']");
+	By password = By.cssSelector("[type='password']");
+	By login = By.cssSelector("[value='Log In']");
+
 	public LoginPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
-		
-		this.driver=driver;
-		
+
+		this.driver = driver;
+
 	}
 
-
-
-
-	public WebElement getEmail()
-	{
+	public WebElement getEmail() {
 		return driver.findElement(email);
 	}
-	
 
-	public WebElement getPassword()
-	{
+	public WebElement getPassword() {
 		return driver.findElement(password);
 	}
-	
-	public WebElement getLogin()
-	{
+
+	public WebElement getLogin() {
 		return driver.findElement(login);
 	}
-	
-	
-	
+	public WebElement getLogin1() {
+		return driver.findElement(login);
+	}
+	public WebElement getLogin2() {
+		return driver.findElement(login);
+	}
+
 }
